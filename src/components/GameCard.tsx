ab83ed5b91755a43d90ebf273181b0d2724b03f2
fateme kholousi <fatemekholousizaer@gmail.com>
2023-05-01 10:58:54 +0430

    fix: platform icons added to card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import {
-  BsNintendoSwitch,
-  BsPlaystation,
-  BsXbox,
-  BsApple,
-  BsAndroid,
-  BsPCircle,
-} from "react-icons/bs";
-import { RiMacbookLine } from "react-icons/ri";
-import { DiLinux } from "react-icons/di";
-import {
   Image,
-  Text,
   Card,
   CardBody,
   Heading,
   Badge,
   HStack,
 } from "@chakra-ui/react";
+import PlatformIconsList from "./PlatformIconsList";
 
 function GameCard({
   gameCover,
@@ -25,47 +15,17 @@ function GameCard({
   title = "",
   playTime,
 }: IGameCard) {
-  console.log(supportedServices);
-  const gamePlatformIcons = {
-    mac: <RiMacbookLine />,
-    playstation: <BsPlaystation />,
-    xbox: <BsXbox />,
-    pc: <BsPCircle />,
-    android: <BsAndroid />,
-    apple: <BsApple />,
-    nintendo: <BsNintendoSwitch />,
-    linux: <DiLinux />,
-  };
-
-  function gamePlatformIconRecognizer(name: string) {
-    const simpleName = name
-      .toLowerCase()
-      .trim()
-      .replace(" ", "")
-      .replace("-", "");
-
-    if (simpleName.startsWith("mac")) return gamePlatformIcons["mac"];
-    else if (simpleName.startsWith("playstation"))
-      return gamePlatformIcons["playstation"];
-    else if (simpleName.startsWith("xbox")) return gamePlatformIcons["xbox"];
-    else if (simpleName.startsWith("pc")) return gamePlatformIcons["pc"];
-    else if (simpleName.startsWith("android"))
-      return gamePlatformIcons["android"];
-    else if (simpleName.startsWith("apple")) return gamePlatformIcons["apple"];
-    else if (simpleName.startsWith("nintendo"))
-      return gamePlatformIcons["nintendo"];
-    else if (simpleName.startsWith("linux")) return gamePlatformIcons["linux"];
-  }
-
   return (
     <>
       <Card maxW="sm" borderRadius="1rem" variant={"filled"} overflow="hidden">
         <Image src={gameCover} alt="game cover" />
         <CardBody>
           <HStack mb={4} justifyContent={"space-between"}>
-            <HStack wrap="wrap">
+            <HStack wrap="wrap" alignItems="center">
               {supportedServices.slice(0, 3).map((item, index) => (
-                <>{gamePlatformIconRecognizer(item.platform.slug)}</>
+                <>
+                  <PlatformIconsList platform={item.platform} />
+                </>
               ))}
             </HStack>
 
---
+++ src/components/PlatformIconsList.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BsNintendoSwitch,
+  BsXbox,
+  BsApple,
+  BsAndroid,
+  BsPCircle,
+} from "react-icons/bs";
+import { RiMacbookLine, RiXboxFill } from "react-icons/ri";
+import { DiLinux } from "react-icons/di";
+import { SiPlaystation3, SiPlaystation4, SiPlaystation5 } from "react-icons/si";
+import { IconType } from "react-icons";
+import { Icon } from "@chakra-ui/react";
+
+function PlatformIconsList({
+  platform,
+}: {
+  platform: { slug: string; name: string };
+}) {
+  const gamePlatformIcons: { [key: string]: IconType } = {
+    macos: RiMacbookLine,
+    playstation4: SiPlaystation4,
+    "xbox-one": BsXbox,
+    pc: BsPCircle,
+    android: BsAndroid,
+    apple: BsApple,
+    "nintendo-switch": BsNintendoSwitch,
+    linux: DiLinux,
+    "xbox-series-x": RiXboxFill,
+    playstation5: SiPlaystation5,
+    playstation3: SiPlaystation3,
+  };
+
+  return <Icon as={gamePlatformIcons[platform.slug]} />;
+}
+
+export default PlatformIconsList;
