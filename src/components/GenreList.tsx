5f051aad9720fc227b274e49eaaa40129e856d30
fateme kholousi <fatemekholousizaer@gmail.com>
2023-05-02 19:27:31 +0430

    fix:genre list images added

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import useGenre from "../hooks/useGenres";
-import { Box } from "@chakra-ui/react";
+import { Text, HStack, List, ListItem, Image } from "@chakra-ui/react";
+import { getCroppedImageUrl } from "../assets/getCroppedImageUrl";
 
 function GenreList() {
   const { data, error, isLoading } = useGenre();
   return (
-    <Box>
-      <ul>
-        {data?.map((genre: { name: string }) => (
-          <li>{genre.name}</li>
-        ))}
-      </ul>
-    </Box>
+    <List>
+      {data?.map((genre) => (
+        <ListItem key={genre.id} py=".25rem">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              borderRadius=".5rem"
+              boxSize="32px"
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 }
 
